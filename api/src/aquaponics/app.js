eb9954ad3c722d5ea2e621066ca3055f90dbb811
petter <petterhgustafsson@gmail.com>
2019-02-03 18:18:42 +0100

    Add /healthy endpoint

--- api/src/aquaponics/app.js
+++ api/src/aquaponics/app.js
@@ -7,12 +7,17 @@ app.use(bodyParser.json())
 
 const collection = 'sensors'
 
-app.post('/aquaponics/:ts'), async (req, res) => {
+
+app.get('/healthy', (req, res) => {
+	res.status(200).send()
+})
+
+app.post('/aquaponics/:ts', async (req, res) => {
 	await firestore.add(collection, req.body.data)
 	res.status(200).send()	
-}
+})
 
-app.get('/aquaponics'), async (req, res) => {
+app.get('/aquaponics', async (req, res) => {
 	const data = await firestore.get(collection)
 	res.status(200).send(data)
-}
+})
